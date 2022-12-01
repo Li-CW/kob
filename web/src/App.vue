ed6c6112e73d2a89fce25195c476347b06c3469b
Hasity <licanweid@163.com>
2022-12-01 08:51:51 +0800

    start kob

---
+++ web/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  helloworld
+</template>
+
+<script>
+import axios from "axios"
+export default {
+  name: "App",
+  setup: () => {
+    axios.get("http://127.0.0.1:3000/pk/getbotinfo/")
+      .then((response) => {
+        console.log(response);
+      })
+  }
+
+}
+
+
+</script>
+
+<style>
+body{
+  background-image: url("./assets/background.png");
+  background-size: cover;
+}
+</style>
+
